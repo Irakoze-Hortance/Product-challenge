2e1b730224828973e5637c626aa02472a08344fc
Irakoze-Hortance <hortanceirakoze@gmail.com>
2023-02-15 18:26:25 +0200

    initial commit

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,19 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { BrandsController } from './controllers/brands.controller';
-import { ProductsService } from './services/products.service';
-import { CategoriesService } from './services/categories.service';
-import { BrandsService } from './services/brands.service';
 
 import { Product } from './entities/product.entity';
-import { Brand } from './entities/brand.entity';
-import { Category } from './entities/category.entity';
+import { ProductsService } from './services/products.service';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Brand, Category])],
-  controllers: [ProductsController, CategoriesController, BrandsController],
-  providers: [ProductsService, CategoriesService, BrandsService],
+  imports: [TypeOrmModule.forFeature([Product])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class ProductsModule {}
